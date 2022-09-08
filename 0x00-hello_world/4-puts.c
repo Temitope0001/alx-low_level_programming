2110cbc7a8d5594f90b5b69c74c26c76fa8cf057
Temitope0001 <adesinatemitope0001@gmail.com>
2022-09-08 16:56:33 -0700

    u

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Return: always 0
+ *maini - Entry point
+ *Return: always 0
  */
 int main(void)
 {                                                         puts("\"Programming is like building a multilingual puzzle");
