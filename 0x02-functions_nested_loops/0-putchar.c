efeaa174e45eb48ffa152f6d9bce7d07fe8254b4
Temitope0001 <adesinatemitope0001@gmail.com>
2022-09-13 08:58:30 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
-/**main - Entry point
-*Return: always 0 (success)
+#include <studio.h>
+#include <main.h>
+/**main - enrry point
+*return: always 0
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }
