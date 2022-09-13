4f7672c25a839cdac7ab95cc682f3cd660fedb5a
Temitope0001 <adesinatemitope0001@gmail.com>
2022-09-13 12:21:48 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphaber_x10 - prints alphabet in lowercase ten times
+*
+*Return: x10 a-z (success)
+*/
+void print_alphabet_x10(void)
+{
+	int n, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
