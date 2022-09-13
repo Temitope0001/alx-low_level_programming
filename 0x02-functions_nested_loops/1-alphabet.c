ff5d52c9b50e9174029ae84de16afa0c0cd65d8c
Temitope0001 <adesinatemitope0001@gmail.com>
2022-09-13 11:54:29 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
